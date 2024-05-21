78d9b306e9ed49278ae8ad2752afb8a4f728afae
kiso1 <kislev.levy@gmail.com>
2024-05-21 11:41:20 +0300

    V1.0

---
+++ index.js
@@ -0,0 +1,70 @@
+// Global imports:
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+const templateRender = require("./js-modules/templateRender");
+
+// File imports:
+const data = fs.readFileSync(`${__dirname}/api-data/data.json`, "utf-8");
+const dataObj = JSON.parse(data);
+const productTemplate = fs.readFileSync(
+  `${__dirname}/html-templates/product-template.html`,
+  "utf-8"
+);
+const overviewTemplate = fs.readFileSync(
+  `${__dirname}/html-templates/overview-template.html`,
+  "utf-8"
+);
+const cardTemplate = fs.readFileSync(
+  `${__dirname}/html-templates/card-template.html`,
+  "utf-8"
+);
+const icon = fs.readFileSync(`${__dirname}/images/icon.png`);
+///////////////////////////////////////////////////////////////////
+
+// Create server:
+const server = http.createServer((req, res) => {
+  const { query, pathname: path } = url.parse(req.url, true);
+
+  // Overview page:
+  if (path === "/overview" || path === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const cardHtml = dataObj
+      .map((product) => templateRender(cardTemplate, product))
+      .join("");
+    res.end(overviewTemplate.replace("{%PRODUCT_CARDS%}", cardHtml));
+
+    // Product page:
+  } else if (path === "/product") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const product = dataObj[query.id];
+    res.end(templateRender(productTemplate, product));
+
+    // API page:
+  } else if (path === "/api") {
+    res.writeHead(200, {
+      "Content-type": "application/json",
+    });
+    res.end(data);
+
+    // Icon request:
+  } else if (path === "/icon") {
+    res.writeHead(200, { "Content-type": "image/png" });
+    res.end(icon);
+
+    // Page not found:
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "hello world",
+    });
+    res.end("<h1>Page not found</h1>");
+  }
+});
+
+// Server start:
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Listning on port 8000");
+});
+
+///////////////////////////////////////////////////////////////////
---
+++ js-modules/templateRender.js
@@ -0,0 +1,13 @@
+// Helper functions:
+module.exports = function (template, product) {
+  return template
+    .replace(/{%PRODUCT_NAME%}/g, product.productName)
+    .replace(/{%PRODUCT_IMAGE%}/g, product.image)
+    .replace(/{%PRODUCT_PRICE%}/g, product.price * 4)
+    .replace(/{%PRODUCT_ORIGIN%}/g, product.from)
+    .replace(/{%PRODUCT_NUTRIENTS%}/g, product.nutrients)
+    .replace(/{%PRODUCT_QUANTITY%}/g, product.quantity)
+    .replace(/{%PRODUCT_DESCRIPTION%}/g, product.description)
+    .replace(/{%PRODUCT_ID%}/g, product.id)
+    .replace(/{%IS_ORGANIC%}/g, product.organic ? "" : "not-organic");
+};
